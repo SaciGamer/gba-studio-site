a8e8baa477019af165363a3963a7212492c066c1
SaciGamer <sacigamer.dev+github@gmail.com>
2025-12-04 18:14:33 -0300

    feat: add meta description of site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,7 +34,7 @@ export default function Home(): ReactNode {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="GBA Studio logo <head />">
+      description="A quick and easy to use drag and drop retro game creaator for your favorite games in gba <head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
