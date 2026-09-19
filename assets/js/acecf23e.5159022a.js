6429409dc76770354e8aaefc14e3d23b1faaef40
SaciGamer <sacigamer.dev+github@gmail.com>
2026-09-19 14:40:50 -0300

    Deploy website - based on 258a6ba97a9daeb9e058d9d955b6f918b2df33c9

---
+++ assets/js/acecf23e.5159022a.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkgba_studio_web_page=globalThis.webpackChunkgba_studio_web_page||[]).push([[903],{1912:a=>{a.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
---
+++ es/assets/js/acecf23e.1a9f4e61.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkgba_studio_web_page=globalThis.webpackChunkgba_studio_web_page||[]).push([[903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/es/blog","blogTitle":"Blog","authorsListPath":"/es/blog/authors"}')}}]);
